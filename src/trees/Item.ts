d0ea1b94360835ff8aa807d04158a5aec68faacc
Nazim Lachter <lachtater@gmail.com>
2020-01-29 22:20:08 +0100

    Command debug

--- src/trees/Item.ts
+++ src/trees/Item.ts
@@ -3,16 +3,24 @@ import { VSCodeTreeRecipe } from "./Provider"
 
 export interface VSCodeTreeItemOptions {
   description?: string
+  command?: vscode.Command
 }
 
 export class VSCodeTreeItem extends vscode.TreeItem {
-  constructor(id: string[], label: string, private children?: VSCodeTreeRecipe, options: VSCodeTreeItemOptions = {}) {
-    super(label, vscode.TreeItemCollapsibleState.Collapsed)
+
+  constructor(id: string[], label: string, private children: VSCodeTreeRecipe, options: VSCodeTreeItemOptions = {}) {
+    super(label)
     this.contextValue = id.join("-")
+    if(children.length !== 0) {
+      this.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed
+    } else {
+      this.command = { title: "test", command: "tree.catalog.test", arguments: [ this.contextValue ] }
+    }
     this.description = options.description
   }
 
   getChildren() {
     return this.children
   }
+
 }
