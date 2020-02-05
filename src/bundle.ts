f6b0df3dfe76ec9b100bcf5f9e7c337de32352fe
Nazim Lachter <lachtater@gmail.com>
2020-02-05 19:07:41 +0100

    Commands

--- src/bundle.ts
+++ src/bundle.ts
@@ -2,8 +2,25 @@ import { i18nSettings } from "dropin-recipes"
 import * as vscode from "vscode"
 import { VSCodeTreeParams, VSCodeTreeProvider } from "./trees/Provider"
 
+export type KiwiBundleVSCodeCommand = ((...params: any[]) => void)
+
+export type KiwiBundleVSCodeCommands = { [name: string]: KiwiBundleVSCodeCommands | KiwiBundleVSCodeCommand }
+
 export interface KiwiBundleVSCodeParams {
   trees?: { [treeId: string]: VSCodeTreeParams }
+  commands?: KiwiBundleVSCodeCommands
+}
+
+const registerCommands = (commands: KiwiBundleVSCodeCommands, name: string = "") => {
+  if(typeof commands === "function") {
+    if(name.length !== 0) {
+      vscode.commands.registerCommand(`commands.${name}`, commands)
+    }
+  } else {
+    Object.keys(commands).forEach(childName => {
+      registerCommands(commands[childName] as KiwiBundleVSCodeCommands, `${name}${name.length === 0 ? "" : "."}${childName}`)
+    })
+  }
 }
 
 export const KiwiBundleVSCode = (params: KiwiBundleVSCodeParams) => {
@@ -16,5 +33,10 @@ export const KiwiBundleVSCode = (params: KiwiBundleVSCodeParams) => {
     Object.keys(trees).forEach(treeId => {
       vscode.window.registerTreeDataProvider(treeId, new VSCodeTreeProvider(treeId, trees[treeId]))
     })
+  }
+
+  // Commands
+  if(typeof params.commands !== "undefined") {
+    registerCommands(params.commands)
   }
 }
